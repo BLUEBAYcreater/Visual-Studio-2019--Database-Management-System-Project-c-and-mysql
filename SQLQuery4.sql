8286df87e9a65731aaa05840c786c9544c361b2e
Yeshwanth. p <39979024+BLUEBAYcreater@users.noreply.github.com>
2019-12-02 22:18:06 +0530

    Add files via upload

---
+++ SQLQuery4.sql
@@ -0,0 +1,29 @@
+﻿CREATE TRIGGER [dbo].[PINS]
+	ON [dbo].[acoffee]
+	INSTEAD OF INSERT
+	AS
+	BEGIN
+		SET NOCOUNT ON;
+
+		DECLARE  @Id int, @name nvarchar(50),@type nvarchar(50), @price int
+		DECLARE @MESSAGE VARCHAR(100)
+
+		SELECT @Id = INSERTED.Id,
+		       @name = INSERTED.name,
+			   @type = INSERTED.type,
+			   @price = INSERTED.price
+			   FROM INSERTED
+
+			   IF(@price<0)
+			   BEGIN
+			   SET @MESSAGE = 'INVALID PRICE'
+			   RAISERROR(@MESSAGE,16,1)
+			   END
+			   ELSE
+			   BEGIN
+			   INSERT INTO acoffee VALUES(@Id,@name,@type,@price)
+
+			   END 
+END
+
+
---
+++ SQLQuery5.sql
@@ -0,0 +1,31 @@
+﻿CREATE TRIGGER [dbo].[PINS1]
+	ON [dbo].[acoffee]
+	INSTEAD OF UPDATE
+	AS
+	BEGIN
+		SET NOCOUNT ON;
+
+		DECLARE  @Id int, @name nvarchar(50),@type nvarchar(50), @price int
+		DECLARE @MESSAGE VARCHAR(100)
+
+		SELECT @Id = INSERTED.Id,
+		       @name = INSERTED.name,
+			   @type = INSERTED.type,
+			   @price = INSERTED.price
+			   FROM INSERTED
+
+			   IF(@price<0)
+			   BEGIN
+			   SET @MESSAGE = 'INVALID PRICE'
+			   RAISERROR(@MESSAGE,16,1)
+			   END
+			   ELSE
+			   BEGIN
+			   UPDATE acoffee SET name=@name,type=@type,price=@price where Id=@Id
+
+			   END 
+END
+
+
+
+UPDATE acoffee set PRICE=100 WHERE Id=3
